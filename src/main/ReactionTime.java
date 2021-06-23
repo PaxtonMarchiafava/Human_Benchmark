2c42e626cc34d43bafb0c4a8d00044f433476ba9
Paxton_M <65143728+PaxtonMarchiafava@users.noreply.github.com>
2021-06-23 00:03:40 -0400

    Slightly faster, and shorter.

--- src/main/ReactionTime.java
+++ src/main/ReactionTime.java
@@ -5,9 +5,7 @@ import java.awt.Robot;
 import java.awt.Color;
 import java.awt.event.InputEvent;
 
-
 public class ReactionTime {
-	
 	
 	public static int getx = 450;
 	public static int gety = 350;
@@ -18,16 +16,10 @@ public class ReactionTime {
 	
 	public static boolean waiting = true;
 	
-	//public static Color get = new Color();
-	
-	
-
 	public static void main(String[] args) throws AWTException, InterruptedException {
 		// TODO Auto-generated method stub
 		
 		Robot robot = new Robot(); // create robot instance
-		
-		Color get = new Color(0, 0, 0); // make new color to store detected info 
 		
 		robot.mouseMove(mousex, mousey); // move mouse to where it should click
 		
@@ -37,14 +29,13 @@ public class ReactionTime {
 			Thread.sleep(100); // wait for a little while so the same frame is not detected and clicked more than once
 			waiting = true;
 			robot.mouseMove(mousex, mousey); // move mouse to correct location to click
-			click(robot); // begin the test
+			robot.mousePress(mouseButton);
+			robot.mouseRelease(mouseButton);
 			
 			while (waiting) {
-
-				get = robot.getPixelColor(getx, gety); // get the color as often as possible
-				
-				if (get.getGreen() > get.getRed() && get.getGreen() > 175) { // if screen if green
-					click(robot);
+				if (robot.getPixelColor(450, 350).getGreen() > 175) { // if screen if green
+					robot.mousePress(mouseButton);
+					robot.mouseRelease(mouseButton);
 					waiting = false;
 				}
 				
@@ -53,10 +44,4 @@ public class ReactionTime {
 		}
 		
 	}
-	
-	public static void click(Robot robot) {
-		robot.mousePress(mouseButton);
-		robot.mouseRelease(mouseButton);
-	}
-
 }
